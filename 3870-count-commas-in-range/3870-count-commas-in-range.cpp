a8c154261fa1f4607d0b1f14a242d403a9fb9e21
Ayushmaan Singh <156946735+ayushsingh721@users.noreply.github.com>
2026-09-08 21:25:00 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.49%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long countCommas(int n) {
+        long long ans = 0;
+
+        if (n >= 1000)
+            ans += n - 999;
+
+        if (n >= 1000000)
+            ans += n - 999999;
+
+        return ans;
+    }
+};
