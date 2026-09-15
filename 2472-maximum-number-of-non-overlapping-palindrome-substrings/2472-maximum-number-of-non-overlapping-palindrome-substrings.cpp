d89f83ca07c6490b846a193fd7981d1b65ca315a
Ayushmaan Singh <156946735+ayushsingh721@users.noreply.github.com>
2026-09-15 17:42:26 +0530

    Time: 0 ms (100%), Space: 8.8 MB (98.43%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
+    bool isPal(string &s, int l, int r) {
+        while (l < r) {
+            if (s[l++] != s[r--])
+                return false;
+        }
+        return true;
+    }
+
     int maxPalindromes(string s, int k) {
         int n = s.size();
+        int ans = 0;
 
-        vector<vector<bool>> pal(n, vector<bool>(n, false));
+        for (int i = 0; i + k <= n; ) {
 
-        // Build palindrome table
-        for (int len = 1; len <= n; len++) {
-            for (int l = 0; l + len - 1 < n; l++) {
-                int r = l + len - 1;
-
-                if (s[l] == s[r] &&
-                    (len <= 2 || pal[l + 1][r - 1])) {
-                    pal[l][r] = true;
-                }
+            if (isPal(s, i, i + k - 1)) {
+                ans++;
+                i += k;
+            }
+            else if (i + k < n && isPal(s, i, i + k)) {
+                ans++;
+                i += k + 1;
+            }
+            else {
+                i++;
             }
         }
 
-        // dp[i] = answer for first i characters
-        vector<int> dp(n + 1, 0);
-
-        for (int i = 1; i <= n; i++) {
-
-            // Don't select a palindrome ending here
-            dp[i] = dp[i - 1];
-
-            // Select s[j ... i-1]
-            for (int j = 0; j <= i - k; j++) {
-
-                if (pal[j][i - 1]) {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
-            }
-        }
-
-        return dp[n];
+        return ans;
     }
 };
